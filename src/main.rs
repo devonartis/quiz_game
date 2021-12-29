d5a2e2d448d11a2a4bc79cd6547bbfd06af73334
Devon Artis <562122-devonartis@users.noreply.gitlab.com>
2021-12-29 03:56:46 -0500

    added implmentation for config

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,34 @@ struct Problem {
     answer: String,
 }
 
-
-fn parse_args(args: &[String]) -> &str {
-    
-    let filename = &args[1];
-    
-    filename
+struct Config {
+    filename: String,
 }
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let filename = args[1].clone();
+        Config { filename }
+    }
+}
+
+
+fn get_problems(args: &[String]) -> Config {
+    
+    let filename = args[1].clone();
+    
+    Config{ filename }
+}
+
 fn main() -> Result<(), Error> {
 
     // Get a handle on the arguments from the command line
     // args[1] would be the argument given to the application 
     let args: Vec<String> = env::args().collect();
     
-    let csv_filename = parse_args(&args);
+    let config = Config::new(&args);
 
-    let content = fs::read_to_string(csv_filename)
+    let content = fs::read_to_string(config.filename)
         .expect("Something went wrong with accessing the file");
 
     let mut reader = csv::ReaderBuilder::new()
